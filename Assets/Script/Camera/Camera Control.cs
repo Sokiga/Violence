a7386b20128fdddea198e946829a71e18c095885
leviathanwhite <2843523795@qq.com>
2023-12-12 14:05:50 +0800

    vio

--- Assets/Script/Camera/Camera Control.cs
+++ Assets/Script/Camera/Camera Control.cs
@@ -11,68 +11,29 @@ public class CmeraControl : MonoBehaviour
     [SerializeField] Camera mainCamera;
     public Rigidbody body;
 
-  
+
     private void LateUpdate()
     {
         Vector3 pos = transform.position;
-        pos.x=lookat.transform.position.x;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
+        pos.x = lookat.transform.position.x;
         pos.y = lookat.transform.position.y;
-
-=======
-        pos.y=lookat.transform.position.y;
->>>>>>> parent of f80107e (Text1)
         pos.z = 15;
-=======
-        pos.z=lookat.transform.position.z;
-        pos.y = 15;
->>>>>>> parent of b38d41a (Text)
-=======
-        pos.z=lookat.transform.position.z;
-        pos.y = 15;
->>>>>>> parent of ced1ad7 (vio)
-=======
-        pos.z=lookat.transform.position.z;
-        pos.y = 15;
->>>>>>> parent of ced1ad7 (vio)
         transform.position = pos;
         Vector3 cameraPos = SetCameraPos();
-        transform.position=cameraPos;
-     
+        transform.position = cameraPos;
+
     }
     public Vector3 GetVectorOffset()
     {
-        Vector3 screenCenterPos=new Vector3(Screen.width*0.5f,0, Screen.height * 0.5f);
-        Vector3 mousePos=new Vector3(Input.mousePosition.x,0, Input.mousePosition.y);   
+        Vector3 screenCenterPos = new Vector3(Screen.width * 0.5f, 0, Screen.height * 0.5f);
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y);
         return mousePos - screenCenterPos;
     }
     public Vector3 SetCameraPos()
     {
-        Vector3 offsetPos= GetVectorOffset();
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-        Vector3 cameraPos=new Vector3(lookat.transform.position.x,lookat.transform.position.y,15f);
-
-=======
-        Vector3 cameraPos=new Vector3(lookat.transform.position.x,lookat.transform.position.y,15f);
->>>>>>> parent of f80107e (Text1)
-=======
-        Vector3 cameraPos=new Vector3(lookat.transform.position.x,15,lookat.transform.position.z);
->>>>>>> parent of b38d41a (Text)
-=======
-        Vector3 cameraPos=new Vector3(lookat.transform.position.x,15,lookat.transform.position.z);
->>>>>>> parent of ced1ad7 (vio)
-=======
-        Vector3 cameraPos=new Vector3(lookat.transform.position.x,15,lookat.transform.position.z);
->>>>>>> parent of ced1ad7 (vio)
-        cameraPos=cameraPos+offsetPos*0.001f;
+        Vector3 offsetPos = GetVectorOffset();
+        Vector3 cameraPos = new Vector3(lookat.transform.position.x, lookat.transform.position.y, 15);
+        cameraPos = cameraPos + offsetPos * 0.001f;
         return cameraPos;
-    } 
+    }
 }
--- Assets/Script/Player/Move.cs
+++ Assets/Script/Player/Move.cs
@@ -4,27 +4,16 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+    public float MoveSpead = 5f;
+    public Rigidbody2D rb;
+    Vector3 movement;
     private PlayerController playerController;
     private void Start()
     {
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of f80107e (Text1)
         playerController = GetComponent<PlayerController>();
     }
     public void Run()
     {
-        rb.velocity = MoveSpead * movement;
-<<<<<<< HEAD
-
-        //（修改地方）更改角色控制器移动类型
-
-=======
->>>>>>> parent of f80107e (Text1)
         if (rb.velocity.magnitude > 0.05f)
         {
             playerController.moveType = MoveType.kTrot;
@@ -33,53 +22,10 @@ public class Move : MonoBehaviour
         {
             playerController.moveType = MoveType.kStop;
         }
-=======
-   
-      
-    
-  
-
-    // Start is called before the first frame update
-    public void Run()
-    {
-        rb.velocity = MoveSpead * movement;
->>>>>>> parent of b38d41a (Text)
-=======
-   
-      
-    
-  
-
-    // Start is called before the first frame update
-    public void Run()
-    {
-        rb.velocity = MoveSpead * movement;
->>>>>>> parent of ced1ad7 (vio)
-=======
-   
-      
-    
-  
-
-    // Start is called before the first frame update
-    public void Run()
-    {
-        rb.velocity = MoveSpead * movement;
->>>>>>> parent of ced1ad7 (vio)
     }
     public void SlowMove()
     {
         rb.velocity = MoveSpead * movement / 2;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-        playerController.moveType = MoveType.kCreep;
-
-=======
-        playerController.moveType = MoveType.kCreep;
->>>>>>> parent of f80107e (Text1)
         if (rb.velocity.magnitude > 0.05f)
         {
             playerController.moveType = MoveType.kCreep;
@@ -88,33 +34,12 @@ public class Move : MonoBehaviour
         {
             playerController.moveType = MoveType.kStop;
         }
-<<<<<<< HEAD
-=======
->>>>>>> parent of ced1ad7 (vio)
-=======
->>>>>>> parent of ced1ad7 (vio)
-
-=======
->>>>>>> parent of f80107e (Text1)
-=======
-
->>>>>>> parent of b38d41a (Text)
     }
     public void Dash()
     {
         rb.velocity = MoveSpead * movement * 2;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
         playerController.moveType = MoveType.kRun;
-
-=======
-
         playerController.moveType = MoveType.kRun;
->>>>>>> parent of f80107e (Text1)
         if (rb.velocity.magnitude > 0.05f)
         {
             playerController.moveType = MoveType.kRun;
@@ -123,86 +48,26 @@ public class Move : MonoBehaviour
         {
             playerController.moveType = MoveType.kStop;
         }
-=======
->>>>>>> parent of b38d41a (Text)
-=======
->>>>>>> parent of ced1ad7 (vio)
-=======
->>>>>>> parent of ced1ad7 (vio)
     }
-    public float MoveSpead = 5f;
-    public Rigidbody rb;
-    Vector3 movement;
+
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         if (!playerController.isStopMove)
         {
-<<<<<<< HEAD
-
-            
-            movement.x = -Input.GetAxis("Horizontal");
-
             //检测用户输入 
-            //（修改地方）调整移动方向
-            movement.x = -Input.GetAxis("Horizontal");//会根据输入返回一个-1到1之间的值
-
-=======
-            
-            movement.x = -Input.GetAxis("Horizontal");
->>>>>>> parent of f80107e (Text1)
-            movement.y = Input.GetAxis("Vertical");
+            movement.x = Input.GetAxis("Horizontal");//会根据输入返回一个-1到1之间的值
+            movement.z = Input.GetAxis("Vertical");
         }
         else
         {
             movement = Vector3.zero;
         }
-<<<<<<< HEAD
 
        
     }
-
-=======
-=======
->>>>>>> parent of ced1ad7 (vio)
-        //检测用户输入 
-        movement.x = Input.GetAxis("Horizontal");//会根据输入返回一个-1到1之间的值
-        movement.z = Input.GetAxis("Vertical");
-       
-    //就比如我按下键盘的左箭头，意味着要往左动函数会返回-1
-    //如果是右箭头 意味着会返回-1
-}
-<<<<<<< HEAD
->>>>>>> parent of ced1ad7 (vio)
-=======
->>>>>>> parent of ced1ad7 (vio)
     void FixedUpdate()
     {
-       
-        //行动
-
-=======
-=======
-        //检测用户输入 
-        movement.x = Input.GetAxis("Horizontal");//会根据输入返回一个-1到1之间的值
-        movement.z = Input.GetAxis("Vertical");
->>>>>>> parent of b38d41a (Text)
-       
-    //就比如我按下键盘的左箭头，意味着要往左动函数会返回-1
-    //如果是右箭头 意味着会返回-1
-}
-    void FixedUpdate()
-    {
-<<<<<<< HEAD
-        
->>>>>>> parent of f80107e (Text1)
-=======
-       
-        //行动
->>>>>>> parent of b38d41a (Text)
         if (Input.GetKey(KeyCode.LeftShift))
         {
             Dash();
@@ -215,34 +80,8 @@ public class Move : MonoBehaviour
         {
             Run();
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-     
-
-=======
-      
->>>>>>> parent of ced1ad7 (vio)
-
-
-        //movement*速度*函数与函数被调用之间的时间间隔
-        //恒定速度移动
-
-=======
-        
->>>>>>> parent of f80107e (Text1)
-    }
-}
-=======
-      
->>>>>>> parent of b38d41a (Text)
-=======
-      
->>>>>>> parent of ced1ad7 (vio)
 
         //movement*速度*函数与函数被调用之间的时间间隔
         //恒定速度移动
     }
-
 }
