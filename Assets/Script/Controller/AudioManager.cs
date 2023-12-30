5ba3d34501146dc2228006139eaeb1cb03e698de
leviathanwhite <2843523795@qq.com>
2023-12-30 21:44:34 +0800

    vio

---
+++ Assets/Script/Controller/AudioManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public class Player : MonoBehaviour
+    {
+    public AudioSource music;
+        public AudioClip jump;
+
+        private void Awake()
+        {
+            music = gameObject.AddComponent<AudioSource>();
+            music.playOnAwake = false;
+            jump = Resources.Load<AudioClip>("music/jump");
+        }
+        void Update()
+        {
+            ...
+			if (Input.GetKeyDown(KeyCode.UpArrow))//Èç¹ûÊäÈë¡ü
+            {
+                music.clip = jump;
+                music.Play();
+            }
+        }
+    }
+    }
+
+}
